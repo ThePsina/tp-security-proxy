8ae836fe5bc21a4f48bb1b82c33cbab66cdc21f5
Michael Truschelev <mishele20@mail.ru>
2021-03-06 20:44:53 +0300

    some intercept

--- src/interfaces/interceptor.go
+++ src/interfaces/interceptor.go
@@ -9,6 +9,44 @@ import (
 )
 
 func Intercept(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodConnect {
+		tunnel(w, r)
+		return
+	}
+
+	proxy(w, r)
+}
+
+func proxy(w http.ResponseWriter, r *http.Request) {
+	// #TODO: add writing request to db
+
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer func() {
+		if err = resp.Body.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	// resp.Header returns map[string][]string
+	// key is header key
+	// keyValues is all values in header key
+	for key, keyValues := range resp.Header {
+		for _, val := range keyValues {
+			w.Header().Add(key, val)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	if _, err = io.Copy(w, resp.Body); err != nil {
+		log.Println(err)
+	}
+}
+
+func tunnel(w http.ResponseWriter, r *http.Request) {
 	dstConn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
